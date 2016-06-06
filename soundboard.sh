0a797cc53a0eb84d148dea6f24a52169e2e32b6e
Zachary Matthews <zacharymatt5@gmail.com>
2016-06-06 12:49:35 -0400

    Change arguement system to use getopt

--- soundboard.sh
+++ soundboard.sh
@@ -3,42 +3,63 @@
 # Lock file
 lf=/tmp/sbLockFile
 
-overlap=0
+overlap=false
+cancel=false
+# Getopt
+short=cf:ho
+long=cancel,file:,help,overlap
 
 # Create lockfile if none exists
 cat /dev/null >> $lf
 
 print_usage() {
 	echo -e "Usage: soundboard [OPTION]..." 
-	echo -e "  -h, --help       show this help message"
+	echo -e "  -c, --cancel     allows the selected file to be stoped if playing"
 	echo -e "  -f, --file       file to be played"
+	echo -e "  -h, --help       show this help message"
 	echo -e "  -o, --overlap    allows sound to be played multiple times at once"
-	exit 1
+	exit 0
 }
 
-while [ "$1" ]; do
+getopt --test > /dev/null
+if [[ $? != 4 ]]; then
+	echo "getopt is not functioning as anticipated"
+	exit 1
+fi
+
+parsed=`getopt --options $short --longoptions $long --name "$0" -- "$@"`
+if [[ $? != 0 ]]; then
+	# Getopt not getting correcty aruments
+	exit 2
+fi
+
+eval set -- "$parsed"
+
+while true; do
 	case $1 in
-	-h | --help)
-		# Print help/usage
-		print_usage
-		;;
-	-f | --file)
-		filename="$2"
-		shift
-		;;
-	-o | --overlap)
-		overlap=1
-		;;
-	--*)
-		# Invalid long option
-		echo "Invalid option: $1" >&2
-		exit 1
-		;;
-	-?)
-		# Invalid short option
-		echo "Invalid option: $1" >&2
-		exit 1
-		;;
+		-c|--cancel)
+			cancel=true
+			;;
+		-f|--file)
+			filename="$2"
+			shift
+			;;
+		-h|--help)
+			# Print help/usage
+			print_usage
+			;;
+		-o|--overlap)
+			overlap=true
+			;;
+		--)
+			shift
+			break
+			;;
+		*)
+			# Unknown option
+			echo "Arguement not properly handled"
+			exit 64
+			;;
 	esac
 	shift
 done
@@ -49,12 +70,18 @@ done
 # Checks to see if file is specified and if readable
 if [ -r $filename ] && [ "$filename" != "" ]; then
 	# Plays if filename not in lockfile or if overlap is enabled
-	if ! grep -Fq "$filename" $lf || [ $overlap == 1 ]; then
+	notinlock=! 
+	if ! grep -Fq "$filename" $lf || ($overlap); then
 		echo "$filename $$" >> $lf
-		play -q $filename &> /dev/null
+		# TODO: Why is this surrounded by parens
+		(aplay -q $filename)
 		# Not portable requires GNU sed
 		# Using # delimiter to avoid issues with file path
-		sed -i "#$filename $$#d" $lf
+		sed -i "\#$filename $$#d" $lf
+	elif grep -Fq "$filename" $lf && ($cancel); then
+		pid=$(grep "$filename" $lf | awk -F " " '{print $2}')
+		kill -9 $pid
+		sed -i "\#$filename $pid#d" $lf
 	fi
 else
 	# Doesn't reflect not readable should be rewritten
